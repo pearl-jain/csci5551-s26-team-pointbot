51c0db6eb4e8fb4af4fdc284d36e5e9bb5d48b4b
Adam Yaj <hwvxyeej@gmail.com>
2026-04-07 15:07:03 +0000

    Add basic control package

---
+++ src/control/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='adam',
+    maintainer_email='hwvxyeej@gmail.com',
+    description='Launch all required nodes, run the state machine, control the robot, and peform reasoning.',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
