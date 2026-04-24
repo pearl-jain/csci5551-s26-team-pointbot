8542b9fd55fdce2fb565b943975ebae540c47a92
Adam Yaj <hwvxyeej@gmail.com>
2026-04-24 04:13:40 +0000

    Remove buld log install

--- install/point_bot_interfaces/share/point_bot_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(point_bot_interfaces_IDL_FILES "action/Perception.idl;action/MoveObject.idl")
-set(point_bot_interfaces_INTERFACE_FILES "action/Perception.action;action/MoveObject.action")
